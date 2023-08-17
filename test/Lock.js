8b582c67a7b9e4c86663ab550c9f89870b6eabf9
Yash Thipsay <yashthpsay@gmail.com>
2023-08-17 18:48:54 +0530

    second test

--- test/Lock.js
+++ test/Lock.js
@@ -1,37 +1,53 @@
-const {time, loadFixture,} = require("@nomicfoundation/hardhat-network-helpers");
-const {anyValue} = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
-const {expect} = require("chai");
-const {ethers} = require("hardhat");
+const {
+  time,
+  loadFixture,
+} = require("@nomicfoundation/hardhat-network-helpers");
+const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
-console.log(time);
-console.log(loadFixture);
-console.log(anyValue);
+// console.log(time);
+// console.log(loadFixture);
+// console.log(anyValue);
 
-describe("MyTest", function(){
-  async function runEveryTime(){
+describe("MyTest", function () {
+  async function runEveryTime() {
     const ONE_YEAR_IN_SECONDS = 365 * 24 * 60 * 60;
     const ONE_GWEI = 1000000000;
 
     const lockedAmount = ONE_GWEI;
     const unlockedTime = (await time.latest()) + ONE_YEAR_IN_SECONDS;
 
-    console.log(ONE_YEAR_IN_SECONDS, ONE_GWEI);
-    console.log(unlockedTime);
-
+    // console.log(ONE_YEAR_IN_SECONDS, ONE_GWEI);
+    // console.log(unlockedTime);
 
     const [owner, otherAccounts] = await ethers.getSigners();
-    console.log(owner);
-    console.log(otherAccounts);
+    // console.log(owner);
+    // console.log(otherAccounts);
 
     const MyTest = await ethers.getContractFactory("MyTest");
-    const myTest = await MyTest.deploy(unlockedTime, {value: lockedAmount});
-    
+    const myTest = await MyTest.deploy(unlockedTime, { value: lockedAmount });
 
-    return {myTest, unlockedTime, lockedAmount, owner, otherAccounts};
+    return { myTest, unlockedTime, lockedAmount, owner, otherAccounts };
   }
- 
 
   describe("Deployment", function () {
-    it("")
-  })
-})
+    it("Should check unlocked time", async function () {
+      //The loadFixture method is used to re run a function
+      const { myTest, unlockedTime } = await loadFixture(runEveryTime);
+
+      console.log(unlockedTime);
+      console.log(myTest);
+
+      expect(await myTest.unlockedTime()).to.equal(unlockedTime);
+    });
+    it("Should set the right owner", async function(){
+      const {myTest, owner} = await loadFixture(runEveryTime);
+  
+      expect(await myTest.owner()).to.equal(owner.address);
+    });
+
+    runEveryTime();
+  });
+ 
+});
