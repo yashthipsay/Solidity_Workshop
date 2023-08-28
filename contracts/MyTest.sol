4fad2dc1ef7e522fa289a16f43e502d5104d07b2
Yash Thipsay <yashthpsay@gmail.com>
2023-08-28 10:21:08 +0530

    cross contract calls

--- contracts/MyTest.sol
+++ contracts/MyTest.sol
@@ -5,25 +5,32 @@ pragma solidity ^0.8.9;
 //IMPORT THE HARDHAT CONSOLE
 import "hardhat/console.sol";
 
-contract MyTest{
-    uint256 public unlockedTime;
-    address payable public owner;
+contract ContractA{
+    ContractB immutable public conB; //Creating a cross contract call
+    //any changes done in contract B will change the state of contract A
+    //contract B has context of contract A
+    uint256 public val;
 
-    event Withdrawal(uint256 amount, uint256 when);
+    constructor(address _conB) {
+        conB = ContractB(_conB);
 
-    constructor(uint256 _unlockedTime) payable{
-        require(block.timestamp < _unlockedTime, "Unlocked time invalid");
-
-        unlockedTime = _unlockedTime;
-        owner = payable(msg.sender);
     }
 
-    function withdraw() public {
-        require(block.timestamp >= unlockedTime, "Wait till the deadline is finished");
-        require(msg.sender == owner, "You're not an owner");
+    function callStuff1(bool _fail) external {
+        val = conB.stuff1(_fail);
 
-        emit Withdrawal(address(this).balance, block.timestamp);
+    }
+    uint256 public val2;
+}
 
-        owner.transfer(address(this).balance);
+contract ContractB {
+    uint256 bVal;
+    address public addr;
+    mapping(address => uint256) public balances;
+
+    function stuff1(bool _fail) external returns(uint256) {
+        require(!_fail, "Failed");
+        bVal = 2;
+        return 3;
     }
 }
--- test/Lock.js
+++ test/Lock.js
@@ -6,6 +6,7 @@ const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
+
 // console.log(time);
 // console.log(loadFixture);
 // console.log(anyValue);
